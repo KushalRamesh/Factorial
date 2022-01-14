afd8cf0acda1033dc5cd119cf9f5204b58f705f7
Kushal Ramesh <78539040+Kushalr004@users.noreply.github.com>
2022-01-14 16:03:52 +0530

    Add files via upload

---
+++ JAVA ASSIGNMENT/src/FactorialJava.java
@@ -0,0 +1,13 @@
+public class FactorialJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,
+		fact=1;  
+		  int num=6;   
+		  for(a=1;a<=num;a++){    
+		      fact=fact*a;    
+		  }    
+		  System.out.println("Factorial of 6 is:" +fact);    
+		 }  
+		} 
